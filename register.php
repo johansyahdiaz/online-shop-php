e28b48abc42736ed318e95516c5d1cb42815a430
Johansyah Diaz <johansyah.diaz8@gmail.com>
2025-03-05 18:41:29 +0700

    feat(registrasi): add koneksi, registrasi, field check

--- register.php
+++ register.php
@@ -1,27 +1,40 @@
 <div id="container-user-akses">
     <form action="<?php echo BASE_URL . "proses_register.php"; ?>" method="post">
+
+        <?php
+        $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
+        $nama_lengkap = isset($_GET['nama_lengkap']) ? $_GET['nama_lengkap'] : false;
+        $email = isset($_GET['email']) ? $_GET['email'] : false;
+        $phone = isset($_GET['phone']) ? $_GET['phone'] : false;
+        $alamat = isset($_GET['alamat']) ? $_GET['alamat'] : false;
+
+        if ($notif == "require") {
+            echo "<div class='notif'>Maaf, Kamu harus melengkapi form dibawah ini</div>";
+        }
+        ?>
+
         <div class="element-form">
             <label for="nama_lengkap">Nama Lengkap</label>
             <span>
-                <input type="text" name="nama_lengkap">
+                <input type="text" name="nama_lengkap" value="<?php echo $nama_lengkap ?>">
             </span>
         </div>
         <div class="element-form">
             <label for="email">Email</label>
             <span>
-                <input type="text" name="email">
+                <input type="text" name="email" value="<?php echo $email ?>">
             </span>
         </div>
         <div class="element-form">
             <label for="phone">Nomer Telepon</label>
             <span>
-                <input type="text" name="phone">
+                <input type="text" name="phone" value="<?php echo $phone ?>">
             </span>
         </div>
         <div class="element-form">
             <label for="alamat">alamat</label>
             <span>
-                <textarea name="alamat"></textarea>
+                <textarea name="alamat"><?php echo $alamat ?></textarea>
             </span>
         </div>
         <div class="element-form">
@@ -31,9 +44,9 @@
             </span>
         </div>
         <div class="element-form">
-            <label for="re-password">Re-Type Password</label>
+            <label for="re_password">Re-Type Password</label>
             <span>
-                <input type="password" name="re-password">
+                <input type="password" name="re_password">
             </span>
         </div>
         <div class="element-form">
