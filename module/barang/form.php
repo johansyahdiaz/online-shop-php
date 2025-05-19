c9a0dbf2eb1391d98e81e68b95c69785b169ae4a
Johansyah Diaz <johansyah.diaz8@gmail.com>
2025-05-19 20:23:28 +0700

    feat: lanjut form 1/9

--- module/barang/form.php
+++ module/barang/form.php
@@ -2,7 +2,10 @@
 
 $barang_id = isset($_GET['barang_id']) ? $_GET['barang_id'] : false;
 
-// $kategori = "";
+$nama_barang = "";
+$spesifikasi = "";
+$stok = "";
+$harga = "";
 $status = "";
 $button = "Add";
 
@@ -16,20 +19,45 @@ $button = "Add";
 // }
 
 ?>
-<form action="<?php echo BASE_URL . "module/barang/action.php?barang_id=$barang_id"; ?>" method="POST">
+<form action="<?php echo BASE_URL . "module/barang/action.php?barang_id=$barang_id"; ?>" method="POST" enctype="multipart/form-data">
 
     <div class="element-form">
         <label>Kategori</label>
         <span>
             <select name="kategori_id">
                 <?php
-                $query = mysqli_query($koneksi, "SELECT * FROM kategori WHERE status='on'");
+                $query = mysqli_query($koneksi, "SELECT kategori_id, kategori FROM kategori WHERE status='on' ORDER BY kategori ASC");
                 while ($row = mysqli_fetch_assoc($query)) {
                     echo "<option value='$row[kategori_id]'> $row[kategori]</option>";
                 }
                 ?>
             </select>
         </span>
+    </div>
+
+    <div class="element-form">
+        <label>Nama Barang</label>
+        <span><input type="text" name="nama_barang" value="<?php echo $nama_barang; ?>" /></span>
+    </div>
+
+    <div class="element-form">
+        <label>Spesifikasi</label>
+        <span><textarea name="spesifikasi"><?php echo $spesifikasi; ?></textarea></span>
+    </div>
+
+    <div class="element-form">
+        <label>Stok</label>
+        <span><input type="text" name="stok" value="<?php echo $stok; ?>" /></span>
+    </div>
+
+    <div class="element-form">
+        <label>Harga</label>
+        <span><input type="text" name="harga" value="<?php echo $harga; ?>" /></span>
+    </div>
+
+    <div class="element-form">
+        <label>Gambar Produk</label>
+        <span><input type="file" name="file" /></span>
     </div>
 
     <div class="element-form">
