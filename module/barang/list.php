1a762350ad359d865a6d114f4622e599e6ce200d
Johansyah Diaz <johansyah.diaz8@gmail.com>
2025-05-20 07:50:59 +0700

    feat: join 5/9

--- module/barang/list.php
+++ module/barang/list.php
@@ -3,7 +3,7 @@
 </div>
 
 <?php
-$query = mysqli_query($koneksi, "SELECT * FROM barang");
+$query = mysqli_query($koneksi, "SELECT barang.*, kategori.kategori  FROM barang JOIN kategori ON barang.kategori_id=kategori.kategori_id ORDER BY nama_barang ASC");
 
 if (mysqli_num_rows($query) == 0) {
     echo "<h3>Saat ini belum ada barang di dalam table barang </h3>";
@@ -13,6 +13,7 @@ if (mysqli_num_rows($query) == 0) {
     echo "<tr class='baris-title'>
     <th class='kolom-nomor'>No</th>
     <th class='kiri'>Barang</th>
+    <th class='kiri'>Kategori</th>
     <th class='kiri'>Harga</th>
     <th class='tengah'>Status</th>
     <th class='tengah'>Action</th>
@@ -25,6 +26,7 @@ if (mysqli_num_rows($query) == 0) {
     <tr>
     <td class='kolom-nomor'>$no</td>
     <td class='kiri'>$row[nama_barang]</td>
+    <td class='kiri'>$row[kategori]</td>
     <td class='kiri'>$row[harga]</td>
     <td class='tengah'>$row[status]</td>
     <td class='tengah'>
