81b16dc3d10bd88acd57472878401e8cfe60ac0e
Johansyah Diaz <johansyah.diaz8@gmail.com>
2025-05-21 20:18:05 +0700

    style: css list barang

--- main.php
+++ main.php
@@ -18,25 +18,36 @@
     <div id="frame-barang">
         <ul>
             <?php
-            $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status = 'on'");
 
+
+
+            $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status = 'on' ORDER BY barang_id DESC LIMIT 9");
+
+
+
+            $no = 1;
             while ($row = mysqli_fetch_assoc($query)) {
 
-                echo "<li>
-    <p class='price'>" . rupiah($row['harga']) . "</p>
-    <a href='" . BASE_URL . "index.php?page=detail&barang_id=" . $row['barang_id'] . "'>
-        <img src='" . BASE_URL . "images/barang/" . $row['gambar'] . "'/>
-    </a>
-    <div class='keterangan-gambar'>
-        <p>
-            <a href='" . BASE_URL . "index.php?page=detail&barang_id=" . $row['barang_id'] . "'>" . $row['nama_barang'] . "</a>
-        </p>
-        <span>Stok: " . $row['stok'] . "</span>
-    </div>
-    <div class='button-add-chart'>
-        <a href='" . BASE_URL . "tambah_keranjang.php?barang_id=" . $row['barang_id'] . "'>+ add to chart</a>
-    </div>
-</li>";
+                $style = false;
+                if ($no == 3) {
+                    $style = "style='margin-right:0px'";
+                    $no = 0;
+                }
+
+                echo "<li $style>
+							<p class='price'>" . rupiah($row['harga']) . "</p>
+							<a href='" . BASE_URL . "index.php?page=detail&barang_id=$row[barang_id]'>
+								<img src='" . BASE_URL . "images/barang/$row[gambar]' />
+							</a>
+							<div class='keterangan-gambar'>
+								<p><a href='" . BASE_URL . "index.php?page=detail&barang_id=$row[barang_id]'>$row[nama_barang]</a></p>
+								<span>Stok : $row[stok]</span>
+							</div>
+							<div class='button-add-cart'>
+								<a href='" . BASE_URL . "tambah_keranjang.php?barang_id=$row[barang_id]'>+ add to cart</a>
+							</div>";
+
+                $no++;
             }
             ?>
         </ul>
